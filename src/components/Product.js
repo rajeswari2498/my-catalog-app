b2ed2d28d3e2ce4fd965e59e652d32f89b8e7b49
rajeswari2498 <rajeswariyesasvini@msitprogram.net>
2022-08-29 21:32:52 +0530

    updated react

---
+++ src/components/Product.js
@@ -0,0 +1,104 @@
+const Product = [
+
+    {
+        id : 1,
+        title : "  Books",
+        price : 250, 
+        category : "Books",
+        image: "./images/book1.jpg"
+
+    },
+    {
+        id : 2,
+        title : "  Books",
+        price : 250, 
+        category : "Books",
+        image: "./images/book2.png"
+
+    },
+    {
+        id : 3,
+        title : "  Books",
+        price : 250, 
+        category : "Books",
+        image: "./images/book3.jpg"
+
+    },
+    {
+        id : 4,
+        title : " Books",
+        price : 250,
+        category : "Books", 
+        image: "./images/book5.jpg"
+
+    },
+    {
+        id : 5,
+        title : "Cosmetics ",
+        price : 750, 
+        category : "Cosmetics",
+        image: "./images/cosmetic2.png"
+
+    },
+    {
+        id : 6,
+        title : "Cosmetics",
+        price : 1050, 
+        category : "Cosmetics",
+        image: "./images/cosmetic3.jpg"
+
+    },
+
+    {
+        id : 7,
+        title : "Rings",
+        price : 10000, 
+        category : "Rings",
+        image: "./images/ring1.jpg"
+
+    },
+    {
+        id : 8,
+        title : "Rings",
+        price : 50000, 
+        category : "Rings",
+        image: "./images/ring2.jpeg"
+
+    },
+    {
+        id : 9,
+        title : "Rings",
+        price : 250000, 
+        category : "Rings",
+        image: "./images/ring3.jpg"
+
+    },
+    {
+        id : 10,
+        title : "Rings",
+        price : 500000, 
+        category : "Rings",
+        image: "./images/ring4.jpg"
+
+    },
+    {
+        id : 11,
+        title : "Bags",
+        price : 700, 
+        category : "Bags",
+        image: "./images/bag1.jpg"
+
+    },
+    {
+        id : 12,
+        title : "Bags",
+        price : 500, 
+        category : "Bags",
+        image: "./images/bag2.jpg"
+
+    },
+
+    
+]
+
+export default Product;
